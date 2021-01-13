e47cd62db0777970edc537685ddf7774a0e6eb1d
ryoshou <trantien308@gmail.com>
2021-01-13 13:56:07 +0700

    + Edit Turtorial

--- Assets/Scripts/turtorial.cs
+++ Assets/Scripts/turtorial.cs
@@ -9,39 +9,64 @@ public class turtorial : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        b.SetActive(false);
-        c.SetActive(false);
+        SetFalse();
+        a.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(i==0)
-        {
+        // if(i==0)
+        // {
+        //     a.SetActive(true);
+        //     b.SetActive(false);
+        //     c.SetActive(false);
+        // }    
+        // if(i==1)
+        // {
+        //     b.SetActive(true);
+        //     a.SetActive(false);
+        // }    
+        // if(i==2)
+        // {
+        //     b.SetActive(false);
+        //     c.SetActive(true);
+        // }   
+        // if(i==3)
+        // {
+        //     c.SetActive(false);
+        //     gameObject.SetActive(false);
+        //     i = 0;
+        // }    
+    }
+    private void SetFalse()
+    {
+        a.SetActive(false);
+        b.SetActive(false);
+        c.SetActive(false);
+    }
+    private void Check()
+    {
+        SetFalse();
+        if (i==0)
             a.SetActive(true);
-            b.SetActive(false);
-            c.SetActive(false);
-        }    
-        if(i==1)
-        {
+        if (i==1)
             b.SetActive(true);
-            a.SetActive(false);
-        }    
-        if(i==2)
-        {
-            b.SetActive(false);
+        if (i==2)
             c.SetActive(true);
-        }   
-        if(i==3)
-        {
-            c.SetActive(false);
-            gameObject.SetActive(false);
-            i = 0;
-        }    
     }
     public void next()
     {
-        if (i <=2)
+        if (i <2)
             i++;
-    }    
+        Check();
+    }   
+    public void previous()
+    {
+        if (i>0)
+        {
+            i--;
+        }
+        Check();
+    }
 }
